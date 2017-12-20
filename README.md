2c943469787953e4262b77da5eced03939a11764
Yashwanth70 <yash.mydam@gmail.com>
2017-12-20 18:54:44 +0530

    Task of NuWed

---
+++ README.md
@@ -0,0 +1,21 @@
+# NuWed-Task
+
+
+
+```
+npm install or yarn install
+```
+
+**developement**
+
+```
+npm run dev or yarn run dev
+```
+```
+and open the url in browser: http://localhost:8080/
+```
+**production**
+
+```
+npm start
+```
---
+++ src/Loader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './css/loader.css';
+
+
+
+
+const Loader = () => {
+
+    return (
+      <div className="topp">
+      <div className='loader'>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      <div className='circle'></div>
+      
+    </div>
+    </div>
+      )
+  }
+
+export default Loader;
